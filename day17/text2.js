ed503ddcc770ebe6393e0f29fff05b2bb17cb3b8
move-gyu <tpdmsehdrb@naver.com>
2025-08-05 17:42:31 +0900

    day17수업

---
+++ day17/text2.js
@@ -0,0 +1,8 @@
+//dom구축이 완료된 후
+document.addEventListener("DOMContentLoaded", function(){
+
+    const title=document.getElementById("title");
+    title.textContent="title~~";
+});
+//DOM을 포함한 모든 리소스 완료된 후
+//window.addEventListener("load",function(){})
